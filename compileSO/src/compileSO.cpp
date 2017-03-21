b53a4d98dbb82fc59bbdf05f95dfb2b269c35b58
oz <oz@oz-Satellite-L800.mshome.net>
2017-03-21 17:44:32 +0800

    Create project of compileSO

---
+++ compileSO/src/compileSO.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : compileSO.cpp
+// Author      : oz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
